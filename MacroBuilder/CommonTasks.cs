dd7217780b11bf87140729a49e317a90892cf715
Domen988 <zagar.domen@gmail.com>
2014-06-30 20:00:52 +0200

    Delete macro after build&run

---
+++ MacroBuilder/CommonTasks.cs
@@ -0,0 +1,129 @@
+﻿namespace TeklaMacroBuilder
+{
+	/// <summary>
+	/// Common tasks library.
+	/// </summary>
+	public static class CommonTasks
+	{
+		/// <summary>
+		/// Performs numbering.
+		/// </summary>
+		/// <param name="numberAllModified">Indicates whether to perform numbering for all modified parts
+		/// or only to selected modified parts.</param>
+		public static void PerformNumbering(bool numberAllModified)
+		{
+			if (numberAllModified)
+			{
+				new MacroBuilder()
+					.Callback("acmd_partnumbers_all", "", "main_frame")
+					.Run();
+			}
+			else
+			{
+				new MacroBuilder()
+					.Callback("acmd_partnumbers_selected", "", "main_frame")
+					.Run();
+			}
+		}
+
+		/// <summary>
+		/// Opens the numbering settings dialog.
+		/// </summary>
+		public static void OpenNumberingSettings()
+		{
+			new MacroBuilder()
+				.Callback("acmd_display_partnumbers_set_options", "", "main_frame")
+				.Run();
+		}
+
+		/// <summary>
+		/// Opens the drawing list.
+		/// </summary>
+		public static void OpenDrawingList()
+		{
+			new MacroBuilder()
+				.Callback("gdr_menu_select_active_draw", "", "main_frame")
+				.Run();
+		}
+
+		/// <summary>
+		/// Opens single part drawing properties dialog.
+		/// </summary>
+		/// <param name="name">Drawing name.</param>
+		public static void OpenSinglePartDrawingProperties(string name)
+		{
+			new MacroBuilder()
+				.Callback("acmd_display_attr_dialog", "wdraw_dial", "main_frame")
+				.ValueChange("wdraw_dial", "gr_wdraw_get_menu", name)
+				.PushButton("gr_wdraw_get", "wdraw_dial")
+				.Run();
+		}
+
+		/// <summary>
+		/// Opens assembly drawing properties dialog.
+		/// </summary>
+		/// <param name="name">Drawing name.</param>
+		public static void OpenAssemblyDrawingProperties(string name)
+		{
+			new MacroBuilder()
+				.Callback("acmd_display_attr_dialog", "adraw_dial", "main_frame")
+				.ValueChange("adraw_dial", "gr_adraw_get_menu", name)
+				.PushButton("gr_adraw_get", "adraw_dial")
+				.Run();
+		}
+
+		/// <summary>
+		/// Opens cast unit drawing properties dialog.
+		/// </summary>
+		/// <param name="name">Drawing name.</param>
+		public static void OpenCastUnitDrawingProperties(string name)
+		{
+			new MacroBuilder()
+				.Callback("acmd_display_attr_dialog", "cudraw_dial", "main_frame")
+				.ValueChange("cudraw_dial", "gr_cudraw_get_menu", name)
+				.PushButton("gr_cudraw_get", "cudraw_dial")
+				.Run();
+		}
+
+		/// <summary>
+		/// Opens general arrangement drawing properties dialog.
+		/// </summary>
+		/// <param name="name">Drawing name.</param>
+		public static void OpenGeneralArrangementDrawingProperties(string name)
+		{
+			new MacroBuilder()
+				.Callback("acmd_display_attr_dialog", "gdraw_dial", "main_frame")
+				.ValueChange("gdraw_dial", "gr_gdraw_get_menu", name)
+				.PushButton("gr_gdraw_get", "gdraw_dial")
+				.Run();
+		}
+
+		/// <summary>
+		/// Opens auto drawing script editor.
+		/// </summary>
+		/// <param name="name">Script name.</param>
+		public static void OpenAutoDrawingScript(string name)
+		{
+			new MacroBuilder()
+				.Callback("acmd_create_drawings_auto", "", "main_frame")
+				.ListSelect("dia_auto_drawings", "auto_drawings_list", name)
+				.PushButton("Pushbutton_133", "dia_auto_drawings")
+				.Run();
+		}
+
+		/// <summary>
+		/// Creates a general arrangement drawing from template.
+		/// </summary>
+		/// <param name="name">Template name.</param>
+		public static void CreateGeneralArrangementDrawingFromTemplate(string name)
+		{
+			new MacroBuilder()
+				.Callback("acmd_create_dim_general_assembly_drawing", "", "main_frame")
+				.PushButton("Pushbutton", "Create GA-drawing")
+				.ValueChange("gdraw_dial", "gr_gdraw_get_menu", name)
+				.PushButton("gr_gdraw_get", "gdraw_dial")
+				.PushButton("gr_gdraw_ok", "gdraw_dial")
+				.Run();
+		}
+	}
+}
---
+++ MacroBuilder/MacroBuilder.cs
@@ -0,0 +1,427 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+
+using Tekla.Structures;
+using Tekla.Structures.Drawing;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.Operations;
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//
+// Made by Carlos Herrero, Tekla corporation
+// https://extranet.tekla.com/FORUM/default.aspx?g=posts&t=5208
+//
+// Changed by Domen Zagar, June 2014, zagar.domen@gmail.com
+// changes made:
+// - delete macro files after runing macro. Deletes .cs, .dll, .pdb
+//
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+namespace TeklaMacroBuilder
+{
+	/// <summary>
+	/// Macro builder.
+	/// </summary>
+	public sealed class MacroBuilder
+	{
+		#region Constructors
+		/// <summary>
+		/// Initializes a new instance of the class.
+		/// </summary>
+		public MacroBuilder()
+		{
+			this._Macro = new StringBuilder();
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the class.
+		/// </summary>
+		/// <param name="script">Script text.</param>
+		public MacroBuilder(string script)
+		{
+			this._Macro = new StringBuilder(script);
+		}
+		#endregion
+
+		#region Methods
+		/// <summary>
+		/// Activates a field on a dialog.
+		/// </summary>
+		/// <param name="dialog">Dialog name.</param>
+		/// <param name="field">Field name.</param>
+		/// <returns>Reference to self for fluent interface pattern.</returns>
+		public MacroBuilder Activate(string dialog, string field)
+		{
+			return AppendMethodCall("Activate", dialog, field);
+		}
+
+		/// <summary>
+		/// Invokes a callback.
+		/// </summary>
+		/// <param name="callback">Callback name.</param>
+		/// <returns>Reference to self for fluent interface pattern.</returns>
+		public MacroBuilder Callback(string callback)
+		{
+			if(callback == null) throw new ArgumentNullException("callback");
+			return Callback(callback, "");
+		}
+
+		/// <summary>
+		/// Invokes a callback.
+		/// </summary>
+		/// <param name="callback">Callback name.</param>
+		/// <param name="parameter">Callback parameter.</param>
+		/// <returns>Reference to self for fluent interface pattern.</returns>
+		public MacroBuilder Callback(string callback, string parameter)
+		{
+			return Callback(callback, parameter, "main_frame");
+		}
+
+		/// <summary>
+		/// Invokes a callback.
+		/// </summary>
+		/// <param name="callback">Callback name.</param>
+		/// <param name="parameter">Callback parameter.</param>
+		/// <param name="frame">Target frame.</param>
+		/// <returns>Reference to self for fluent interface pattern.</returns>
+		public MacroBuilder Callback(string callback, string parameter, string frame)
+		{
+			return AppendMethodCall("Callback", callback, parameter, frame);
+		}
+
+		/// <summary>
+		/// Checks or unchecks a field.
+		/// </summary>
+		/// <param name="name">Field name.</param>
+		/// <param name="value">Check value.</param>
+		/// <returns>Reference to self for fluent interface pattern.</returns>
+		public MacroBuilder CheckValue(string name, int value)
+		{
+			return AppendMethodCall("CheckValue", name, value);
+		}
+
+		/// <summary>
+		/// Starts a command.
+		/// </summary>
+		/// <param name="command">Command name.</param>
+		/// <param name="parameter">Command parameter.</param>
+		/// <param name="frame">Target frame.</param>
+		/// <returns>Reference to self for fluent interface pattern.</returns>
+		public MacroBuilder CommandStart(string command, string parameter, string frame)
+		{
+			return AppendMethodCall("CommandStart", command, parameter, frame);
+		}
+
+		/// <summary>
+		/// Ends a command.
+		/// </summary>
+		/// <returns>Reference to self for fluent interface pattern.</returns>
+		public MacroBuilder CommandEnd()
+		{
+			return AppendMethodCall("CommandEnd");
+		}
+
+		/// <summary>
+		/// Performs file selection.
+		/// </summary>
+		/// <param name="items">Items to select.</param>
+		/// <returns>Reference to self for fluent interface pattern.</returns>
+		public MacroBuilder FileSelection(params string[] items)
+		{
+			this._Macro.Append("akit.FileSelection(");
+
+			for (int Index = 0; Index < items.Length; Index++)
+			{
+				if (Index > 0)
+				{
+					this._Macro.Append(", ");
+				}
+
+				this._Macro.Append('"').Append(items[Index]).Append('"');
+			}
+
+			this._Macro.AppendLine(");");
+			return this;
+		}
+
+		/// <summary>
+		/// Selects items from a list field.
+		/// </summary>
+		/// <param name="dialog">Dialog name.</param>
+		/// <param name="field">Field name.</param>
+		/// <param name="items">List items.</param>
+		/// <returns>Reference to self for fluent interface pattern.</returns>
+		public MacroBuilder ListSelect(string dialog, string field, params string[] items)
+		{
+			this._Macro.AppendFormat(@"akit.ListSelect(""{0}"", ""{1}""", dialog, field);
+
+			for (int Index = 0; Index < items.Length; Index++)
+			{
+				this._Macro.Append(", ").Append('"').Append(items[Index]).Append('"');
+			}
+
+			this._Macro.AppendLine(");");
+			return this;
+		}
+
+		/// <summary>
+		/// Invokes a modal dialog.
+		/// </summary>
+		/// <param name="value">Modal dialog value.</param>
+		/// <returns>Reference to self for fluent interface pattern.</returns>
+		public MacroBuilder ModalDialog(int value)
+		{
+			return AppendMethodCall("ModalDialog", value);
+		}
+
+		/// <summary>
+		/// Simulates a mouse button down event.
+		/// </summary>
+		/// <param name="frame">Frame name.</param>
+		/// <param name="subframe">Subframe name.</param>
+		/// <param name="x">Mouse X position.</param>
+		/// <param name="y">Mouse Y position.</param>
+		/// <param name="modifier">Mouse button modifiers.</param>
+		/// <returns>Reference to self for fluent interface pattern.</returns>
+		public MacroBuilder MouseDown(string frame, string subframe, int x, int y, int modifier)
+		{
+			return AppendMethodCall("MouseDown", frame, subframe, x, y, modifier);
+		}
+
+		/// <summary>
+		/// Simulates a mouse button up event.
+		/// </summary>
+		/// <param name="frame">Frame name.</param>
+		/// <param name="subframe">Subframe name.</param>
+		/// <param name="x">Mouse X position.</param>
+		/// <param name="y">Mouse Y position.</param>
+		/// <param name="modifier">Mouse button modifiers.</param>
+		/// <returns>Reference to self for fluent interface pattern.</returns>
+		public MacroBuilder MouseUp(string frame, string subframe, int x, int y, int modifier)
+		{
+			return AppendMethodCall("MouseUp", frame, subframe, x, y, modifier);
+		}
+
+		/// <summary>
+		/// Pushes a button.
+		/// </summary>
+		/// <param name="button">Button name.</param>
+		/// <param name="frame">Frame name.</param>
+		/// <returns>Reference to self for fluent interface pattern.</returns>
+		public MacroBuilder PushButton(string button, string frame)
+		{
+			return AppendMethodCall("PushButton", button, frame);
+		}
+
+		/// <summary>
+		/// Changes the active tab page.
+		/// </summary>
+		/// <param name="dialog">Dialog name.</param>
+		/// <param name="field">Field name.</param>
+		/// <param name="item">Tab page.</param>
+		/// <returns>Reference to self for fluent interface pattern.</returns>
+		public MacroBuilder TabChange(string dialog, string field, string item)
+		{
+			return AppendMethodCall("TabChange", dialog, field, item);
+		}
+
+		/// <summary>
+		/// Selects items on a table field.
+		/// </summary>
+		/// <param name="dialog">Dialog name.</param>
+		/// <param name="field">Field name.</param>
+		/// <param name="items">Table items.</param>
+		/// <returns>Reference to self for fluent interface pattern.</returns>
+		public MacroBuilder TableSelect(string dialog, string field, params int[] items)
+		{
+			this._Macro.AppendFormat(@"akit.TableSelect(""{0}"", ""{1}""", dialog, field);
+
+			for (int Index = 0; Index < items.Length; Index++)
+			{
+				this._Macro.Append(", ").Append(items[Index]);
+			}
+
+			this._Macro.AppendLine(");");
+			return this;
+		}
+
+		/// <summary>
+		/// Selects items in a tree field.
+		/// </summary>
+		/// <param name="dialog">Dialog name,</param>
+		/// <param name="field">Field name.</param>
+		/// <param name="rowstring">Tree row string.</param>
+		/// <returns>Reference to self for fluent interface pattern.</returns>
+		public MacroBuilder TreeSelect(string dialog, string field, string rowstring)
+		{
+			return AppendMethodCall("TreeSelect", dialog, field, rowstring);
+		}
+
+		/// <summary>
+		/// Changes a field value.
+		/// </summary>
+		/// <param name="dialog">Dialog name.</param>
+		/// <param name="field">Field name.</param>
+		/// <param name="data">Field value.</param>
+		/// <returns>Reference to self for fluent interface pattern.</returns>
+		public MacroBuilder ValueChange(string dialog, string field, string data)
+		{
+			return AppendMethodCall("ValueChange", dialog, field, data);
+		}
+
+		/// <summary>
+		/// Runs the constructed macro.
+		/// </summary>
+		public void Run()
+		{
+			try
+			{
+				string Name = GetMacroFileName();
+				string MacrosPath = string.Empty;
+				TeklaStructuresSettings.GetAdvancedOption("XS_MACRO_DIRECTORY", ref MacrosPath);
+
+				File.WriteAllText(
+					Path.Combine(MacrosPath, Name),
+					"namespace Tekla.Technology.Akit.UserScript {" +
+						"public class Script {" +
+							"public static void Run(Tekla.Technology.Akit.IScript akit) {" +
+								this._Macro.ToString() +
+							"}" +
+						"}" +
+					"}"
+				);
+
+				RunMacro("..\\" + Name);
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                String NameDll = Name.Substring(0, Name.Length-2) + "dll";                                              // change by Domen Zagar. delete files after runing.
+                String NamePdb = Name.Substring(0, Name.Length-2) + "pdb";
+                File.Delete(Path.Combine(MacrosPath, Name));                                                            
+                File.Delete(Path.Combine(MacrosPath, NameDll));                                                            
+                File.Delete(Path.Combine(MacrosPath, NamePdb));                                                            
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+			}
+			catch (IOException Ex)
+			{
+				Debug.WriteLine(Ex);
+			}
+		}
+
+		/// <summary>
+		/// Executes a macro in the view.
+		/// </summary>
+		/// <param name="macroName">Macro name.</param>
+		public void RunMacro(string macroName)
+		{
+			Model CurrentModel = new Model();
+			DrawingHandler DrawingHandler = new DrawingHandler();
+
+			if (DrawingHandler.GetActiveDrawing() != null)
+				macroName = @"..\drawings\" + macroName;
+
+			if (CurrentModel.GetConnectionStatus())
+			{
+				if (!Path.HasExtension(macroName))
+					macroName += ".cs";
+
+				while (Operation.IsMacroRunning())
+					Thread.Sleep(100);
+
+				Operation.RunMacro(macroName);
+			}
+		}
+
+		/// <summary>
+		/// Returns the constructed macro text.
+		/// </summary>
+		/// <returns>Macro text.</returns>
+		public override string ToString()
+		{
+			return this._Macro.ToString();
+		}
+
+		/// <summary>
+		/// Appends a method call to macro.
+		/// </summary>
+		/// <param name="method">Method to call.</param>
+		/// <param name="arguments">Argument list.</param>
+		/// <returns>Reference to self for fluent interface pattern.</returns>
+		private MacroBuilder AppendMethodCall(string method, params object[] arguments)
+		{
+			this._Macro.Append("akit.").Append(method).Append('(');
+
+			for (int Index = 0; Index < arguments.Length; Index++)
+			{
+				if (Index > 0)
+				{
+					this._Macro.Append(", ");
+				}
+
+				if (arguments[Index] is string)
+				{
+					this._Macro.Append('"').Append(arguments[Index]).Append('"');
+				}
+				else
+				{
+					this._Macro.Append(arguments[Index]);
+				}
+			}
+
+			this._Macro.AppendLine(");");
+			return this;
+		}
+
+		/// <summary>
+		/// Gets the generated macro file name.
+		/// </summary>
+		/// <returns>Generated macro file name.</returns>
+		private static string GetMacroFileName()
+		{
+			lock (Random)
+			{
+				if (_TempFileIndex < 0)
+				{
+					_TempFileIndex = Random.Next(0, MaxTempFiles);
+				}
+				else
+				{
+					_TempFileIndex = (_TempFileIndex + 1) % MaxTempFiles;
+				}
+
+				return string.Format(FileNameFormat, _TempFileIndex);
+			}
+		}
+		#endregion
+
+		#region Fields
+		/// <summary>
+		/// Macro text.
+		/// </summary>
+		private readonly StringBuilder _Macro;
+
+		/// <summary>
+		/// Temporary file index.
+		/// </summary>
+		private static int _TempFileIndex = -1;
+
+		/// <summary>
+		/// Random number source.
+		/// </summary>
+		private static readonly Random Random = new Random();
+
+		/// <summary>
+		/// Maximum number of temporary files to use.
+		/// </summary>
+		private const int MaxTempFiles = 32;
+
+		/// <summary>
+		/// File name format.
+		/// </summary>
+		private const string FileNameFormat = "macro_{0:00}.cs";
+		#endregion
+	}
+}
